54e1e083d47b7889f1174dc3ef7dd40decc3fe78
anca.enache <58123664+ancamiha@users.noreply.github.com>
2021-07-07 01:19:27 +0300

    Login and Register screen added.

--- app/src/main/java/com/example/waterdrinkingapp/MainActivity.java
+++ app/src/main/java/com/example/waterdrinkingapp/MainActivity.java
@@ -1,15 +1,17 @@
 package com.example.waterdrinkingapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private final String TAG = MainActivity.class.getSimpleName();
@@ -19,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     RelativeLayout layout1;
 
     Handler handler = new Handler();
-    Runnable runnable = new Runnable() {
+    Runnable startRunnable = new Runnable() {
         @Override
         public void run() {
             tvTitle.setVisibility(View.GONE);
@@ -39,6 +41,30 @@ public class MainActivity extends AppCompatActivity {
         tvTitle.startAnimation(anim);
 
         layout1 = findViewById(R.id.layer1);
-        handler.postDelayed(runnable, 2000);
+        handler.postDelayed(startRunnable, 2000);
+
+        Button loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Login button clicked");
+                Intent intent = new Intent(v.getContext(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+        Button registerBtn = findViewById(R.id.registerBtn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Register button clicked");
+
+                Intent intent = new Intent(v.getContext(), Register.class);
+//                EditText editText = (EditText) findViewById(R.id.editText);
+//                String message = editText.getText().toString();
+//                intent.putExtra(EXTRA_MESSAGE, message);
+                startActivity(intent);
+            }
+        });
     }
 }
